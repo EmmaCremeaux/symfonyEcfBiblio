67c8b9d8a943fb83514975239be4413d44150f5c
EmmaCremeaux <emma.cremeaux@hotmail.fr>
2023-09-01 11:47:04 +0200

    ADD README

---
+++ README.md
@@ -0,0 +1,62 @@
+# Symfony
+
+Ce repo contient une application de gestion de bibliothèque.
+
+## Prérequis
+
+- Linux, MacOS ou Windows
+- Bash
+- PHP 8
+- composer
+- symfony-cli
+- MariaDB 10
+- docker (optionnel)
+
+## Intallation
+
+```
+git clone https://github.com/EmmaCremeaux/symfonyEcfBiblio
+cd symfony
+composer install
+```
+
+Créez une base de données et un utilisateur dédié pour cette base de données.
+
+## Configuration
+
+Créez un fichier `.env.local` à la racine du projet :
+
+```
+APP_ENV=dev
+APP_Debug=true
+APP_SECRET=465759d33b218807fb0d005fe45b892e
+DATABASE_URL="mysql://symfonyEcfBiblio:123@127.0.0.1:3306/symfonyEcfBiblio?serverVersion=mariadb-10.6.12&charset=utf8mb4"
+```
+
+Pensez à changer la variable `APP_SECRET` et les code d'accès `123` dans la variable `DATABASE_URL`.
+
+**ATTENTION : `APP_SECRET` doit être une chaîne de caractère de 32 caractère en hexadécimal.**
+
+## Migration et fixtures
+
+pour que l'application soit utilisable, vous devez créer le schéma de base de données et charger les données :
+
+```
+bin/dofilo.sh
+```
+
+## Utilisation
+
+Lancez le serveur web de développement : 
+
+```
+symfony serve
+```
+
+puis ouvrez la page suivante : [home page](https://localhost:8000)
+
+## Mentions Légales
+
+Ce projet est sous licence MIT.
+
+la licence est disponible ici : [MIT LICENCE](LICENCE).
--- src/DataFixtures/TestFixtures.php
+++ src/DataFixtures/TestFixtures.php
@@ -6,6 +6,8 @@ use App\Entity\Auteur;
 use App\Entity\Emprunteur;
 use App\Entity\Genre;
 use App\Entity\Livre;
+use App\Entity\Emprunt;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
@@ -34,10 +36,10 @@ class TestFixtures extends Fixture implements FixtureGroupInterface
     {
         $this->manager = $manager;
         $this->loadAuteurs();
-        $this->loadEmprunteurs();
+        $this->loadUsers();
         $this->loadGenres();
         $this->loadLivres();
-
+        $this->loadEmprunts();
     }
 
     public function loadAuteurs() : void
@@ -62,9 +64,6 @@ class TestFixtures extends Fixture implements FixtureGroupInterface
             ],
         ];
 
-        // $auteurRepository = $this->manager->getRepository(Livre::class);
-        // $livres = $auteurRepository->findAll();
-
 
         foreach($datas as $data) {
             $auteur = new Auteur();
@@ -82,62 +81,17 @@ class TestFixtures extends Fixture implements FixtureGroupInterface
 
             $auteur->setPrenom($this->faker->word(1));
 
-            // foreach ($livres as $livre) {
-            //     $this->livres->add($livre);
-            //     $livre->setAuteur($this); 
-            //  }
-
             $this->manager->persist($auteur);
         }
         $this->manager->flush();
     }
 
-    public function loadEmprunteurs() : void
-    {
-        // données statiques
-        $datas = [
-            [
-                'nom' => 'foo',
-                'prenom' => 'foo',
-                'telephone' => '123456789'
-            ],
-            [
-                'nom' => 'bar',
-                'prenom' => 'bar',
-                'telephone' => '123456789'
-            ],
-            [
-                'nom' => 'baz',
-                'prenom' => 'baz',
-                'telephone' => '123456789'
-            ],
-        ];
-
-        foreach($datas as $data) {
-            $emprunteur = new Emprunteur();
-            $emprunteur->setNom($data['nom']);
-            $emprunteur->setPrenom($data['prenom']);
-            $emprunteur->setTelephone($data['telephone']);
-
-            $this->manager->persist($emprunteur);
-        }
-        $this->manager->flush();
-
-        // données dynamiques
-        for($i = 0; $i < 100; $i++) {
-            $emprunteur = new Emprunteur();
-            $emprunteur->setNom($this->faker->word(1));
-            $emprunteur->setPrenom($this->faker->word(1));
-            $emprunteur->setTelephone($this->faker->unique()->randomNumber());
-
-            $this->manager->persist($emprunteur);
-        }
-        $this->manager->flush();
-    }
 
     public function loadGenres() : void
     {
+        
         // données statiques
+        
         $datas = [
             [
                 'nom' => 'poésie',
@@ -205,6 +159,11 @@ class TestFixtures extends Fixture implements FixtureGroupInterface
 
     public function loadLivres() : void
     {
+        $auteurRepository = $this->manager->getRepository(Auteur::class);
+        $auteurs = $auteurRepository->findAll();
+        $genreRepository = $this->manager->getRepository(Genre::class);
+        $genres = $genreRepository->findAll();
+
         // données statiques
         $datas = [
             [
@@ -212,47 +171,50 @@ class TestFixtures extends Fixture implements FixtureGroupInterface
                 'anneeEdition' => '2010',
                 'nombrePages' => '100',
                 'codeIsbn' => '9785786930024',
-                'auteurId' => 1
+                'auteurId' => $auteurs[0],
+                'genres' => $genres[0]
             ],
             [
                 'titre' => 'Consectetur adipiscing elit',
                 'anneeEdition' => '2011',
                 'nombrePages' => '150',
                 'codeIsbn' => '9783817260935',
-                'auteurId' => 2
+                'auteurId' => $auteurs[1],
+                'genres' => $genres[1]
             ],
             [
                 'titre' => 'Mihi quidem Antiochum',
                 'anneeEdition' => '2012',
                 'nombrePages' => '200',
                 'codeIsbn' => '9782020493727',
-                'auteurId' => 3
+                'auteurId' => $auteurs[2],
+                'genres' => $genres[2]
             ],
             [
                 'titre' => 'Quem audis satis belle',
                 'anneeEdition' => '2013',
                 'nombrePages' => '250',
                 'codeIsbn' => '9794059561353',
-                'auteurId' => 4
+                'auteurId' => $auteurs[3],
+                'genres' => $genres[3]
             ],
             
         ];
-        $livreRepository = $this->manager->getRepository(Auteur::class);
-        $auteurs = $livreRepository->findAll();
-
+        
         foreach($datas as $data) {
             $livre = new Livre();
             $livre->setTitre($data['titre']);
             $livre->setAnneeEdition($data['anneeEdition']);
             $livre->setNombrePage($data['nombrePages']);
             $livre->setCodeIsbn($data['codeIsbn']);
-            $livre->getAuteur($data['auteurId']);
-
-
+            $livre->setAuteur($data['auteurId']);
+            $livre->addGenre($data['genres']);            
+            
             $this->manager->persist($livre);
         }
         $this->manager->flush();
-
+        
+        
         // données dynamiques
         for($i = 0; $i < 1000; $i++) {
             $livre = new Livre();
@@ -262,13 +224,168 @@ class TestFixtures extends Fixture implements FixtureGroupInterface
             $livre->setNombrePage($this->faker->randomNumber());
             $livre->setCodeIsbn($this->faker->optional($weight = 0.6)->randomNumber());
 
-            foreach ($auteurs as $auteur) {
+
+            $shortListAuteur = $this->faker->randomElements($auteurs);
+            foreach ($shortListAuteur as $auteur) {
                 $livre->setAuteur($auteur); 
-             }
+            }
+
+            
+            $genresCount = random_int(1, 4);
+            $shortListGenres = $this->faker->randomElements($genres, $genresCount);
+
+            foreach ($shortListGenres as $genre) {
+                $livre->addGenre($genre); 
+            }
 
             $this->manager->persist($livre);
         }
         $this->manager->flush();
+    }
+
+    public function loadEmprunts() : void
+    {
+        $emprunteurRepository = $this->manager->getRepository(Emprunteur::class);
+        $emprunteurs = $emprunteurRepository->findAll();
+        $livreRepository = $this->manager->getRepository(Livre::class);
+        $livres = $livreRepository->findAll();
+
+        // données statiques
+        $datas = [
+            [
+                'dateEmprunt' => new DateTime('2020-02-01 10:00:00'),
+                'dateRetour' => new DateTime('2020-03-01 10:00:00'),
+                'emprunteurId' => $emprunteurs[0],
+                'livreId' => $livres[0]
+            ],
+            [
+                'dateEmprunt' => new DateTime('2020-03-01 10:00:00'),
+                'dateRetour' => new DateTime('2020-04-01 10:00:00'),
+                'emprunteurId' => $emprunteurs[1],
+                'livreId' => $livres[1]
+            ],
+            [
+                'dateEmprunt' => new DateTime('2020-04-01 10:00:00'),
+                'dateRetour' => NULL,
+                'emprunteurId' => $emprunteurs[2],
+                'livreId' => $livres[2]
+            ],
+        ];
+
+        foreach($datas as $data) {
+            $emprunt = new Emprunt();
+            $emprunt->setDateEmprunt($data['dateEmprunt']);
+            $emprunt->setDateRetour($data['dateRetour']);
+            $emprunt->setEmprunteur($data['emprunteurId']);
+            $emprunt->setLivre($data['livreId']);
+
+            $this->manager->persist($emprunt);
+        }
+        $this->manager->flush();
+
+        // données dynamiques
+        for($i = 0; $i < 200; $i++) {
+            $emprunt = new Emprunt();
+            $dateEmprunt = $this->faker->dateTimeBetween('-12 months', '-6 months');
+            $emprunt->setDateEmprunt($dateEmprunt);
+            $dateRetour = $this->faker->optional(0.7)->dateTimeBetween('-5 months', '-1 months');
+            $emprunt->setDateRetour($dateRetour);
+            
+            $shortListEmprunteur = $this->faker->randomElements($emprunteurs);
+            foreach ($shortListEmprunteur as $emprunteur) {
+                $emprunt->setEmprunteur($emprunteur); 
+            }
+
+            $shortListLivre = $this->faker->randomElements($livres);
+            foreach ($shortListLivre as $livre) {
+                $emprunt->setLivre($livre); 
+            }
+            
+
+            $this->manager->persist($emprunt);
+        }
+        $this->manager->flush();
+    }
+
+    public function loadUsers() : void 
+    {
+        $repoEmprunteur = $this->manager->getRepository(Emprunteur::class);
+        $emprunteurs = $repoEmprunteur->findAll();
+
+        // données statiques
+        $datas = [
+            [
+                'email' => 'foo.foo@example.com',
+                'password' => '123',
+                'roles' => ['ROLE_USER'],
+                'enabled' => true,
+                'nom' => 'foo',
+                'prenom' => 'foo',
+                'telephone' => '123456789'
+                
+            ],
+            [
+                'email' => 'bar.bar@example.com',
+                'password' => '123',
+                'roles' => ['ROLE_USER'],
+                'enabled' => false,
+                'nom' => 'bar',
+                'prenom' => 'bar',
+                'telephone' => '123456789'
+            ],
+            [
+                'email' => 'baz.baz@example.com',
+                'password' => '123',
+                'roles' => ['ROLE_USER'],
+                'enabled' => true,
+                'nom' => 'baz',
+                'prenom' => 'baz',
+                'telephone' => '123456789'
+            ]
+        ];
+        foreach ($datas as $data) {
+            $user = new User();
+            $user->setEmail($data['email']);
+            $password = $this->hasher->hashPassword($user, $data['password']);
+            $user->setPassword($password);
+            $user->setRoles($data['roles']);
+            $user->setEnabled($data['enabled']);
+        
+        
+            $this->manager->persist($user); 
+        
+            foreach($datas as $data) {
+                $emprunteur = new Emprunteur();
+                $emprunteur->setNom($data['nom']);
+                $emprunteur->setPrenom($data['prenom']);
+                $emprunteur->setTelephone($data['telephone']);
+
+                $this->manager->persist($emprunteur);
+            }
+            $this->manager->flush();
+
+            // données dynamiques
+            for($i = 0; $i < 100; $i++) {
+                $user = new User();
+                $user->setEmail($this->faker->unique()->safeEmail());
+                $password = $this->hasher->hashPassword($user, '123');
+                $user->setPassword($password);
+                $user->setRoles(['ROLE_USER']);
+                $enabled = [true, false];
+                $user->setEnabled($this->faker->randomElement($enabled));
+
+                $this->manager->persist($user);
+
+                $emprunteur = new Emprunteur();
+                $emprunteur->setNom($this->faker->word(1));
+                $emprunteur->setPrenom($this->faker->word(1));
+                $emprunteur->setTelephone($this->faker->unique()->randomNumber());
+                $emprunteur->setUser($user);
+
+                $this->manager->persist($emprunteur);
+            }
+            $this->manager->flush();
+        }
     }
 
     
