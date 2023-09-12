9aa27ac5862fd50ea5cc75b396b4e580db2ff560
EmmaCremeaux <emma.cremeaux@hotmail.fr>
2023-09-12 11:56:24 +0200

    ADD Livre in TestController

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -25,6 +25,8 @@ class TestController extends AbstractController
         $em = $doctrine->getManager();
         $userRepository = $em->getRepository(User::class);
 
+            // Requêtes de lecture :
+
         // Liste complète de tous les utilisateurs, triée par ordre alphabétique d'email
         $userList = $userRepository->findAll();
 
@@ -48,4 +50,105 @@ class TestController extends AbstractController
             'userNotEnabled' => $userNotEnabled,
         ]);
     }
+
+    #[Route('/livre', name: 'app_test_livre')]
+    public function livre(ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $livreRepository = $em->getRepository(Livre::class);
+
+            // Requêtes de lecture :
+
+        // Liste complète de tous les livres, triée par ordre alphabétique de titre
+        $livreList = $livreRepository->findAll();
+
+        // Données du livre dont l'id est `1`
+        $livre1 = $livreRepository->find(1);
+
+        // Liste des livres dont le titre contient le mot clé `lorem`, triée par ordre alphabétique de titre
+        $loremLivre = $livreRepository->findBookByTitle('lorem');
+
+        // Liste des livres dont l'id de l'auteur est `2`, triée par ordre alphabétique de titre
+        $livreAuteur = $livreRepository->findByAuteur(2);
+
+        // Liste des livres dont le genre contient le mot clé `roman`, triée par ordre alphabétique de titre
+        $genreRepository = $em->getRepository(Genre::class);
+        $genreRoman = $livreRepository->findByGenre('roman');
+
+            // Requêtes de création :
+        $auteurRepository = $em->getRepository(Auteur::class);
+        $auteurSF = $auteurRepository->find(2);
+        $genreSF = $genreRepository->find(6);
+
+            //   - ajouter un nouveau livre
+        $sf = new Livre();
+        //   - titre : Totum autem id externum
+        $sf->setTitre('Totum autem id externum');
+        //   - année d'édition : 2020
+        $sf->setAnneeEdition(2020);
+        //   - nombre de pages : 300
+        $sf->setNombrePage(300);
+        //   - code ISBN : 9790412882714
+        $sf->setCodeIsbn(9790412882714);
+        //   - auteur : Hugues Cartier (id `2`)
+        $sf->setAuteur($auteurSF);
+        //   - genre : science-fiction (id `6`)
+        $sf->addGenre($genreSF);
+
+        $em->persist($sf);
+        $em->flush();
+
+
+            // Requêtes de mise à jour :
+
+        $genreRA = $genreRepository->find(5);
+        //   - modifier le livre dont l'id est `2`
+        $livre2 = $livreRepository->find(2);
+        //   - titre : Aperiendum est igitur
+        $livre2->setTitre('Aperiendum est igitur');
+        //   - genre : roman d'aventure (id `5`)
+        $livre2->addGenre($genreRA);
+        $em->flush();
+
+
+            // Requêtes de suppression :
+        $livre123 = $livreRepository->find(123);
+            //   - supprimer le livre dont l'id est `123`
+        if ($livre123){
+            // Suppression de l'objet
+            $em->remove($livre123);
+            $em->flush();
+        }
+
+        return $this->render('test/livre.html.twig', [
+            'livreList' => $livreList,
+            'livre1' => $livre1,
+            'loremLivre' => $loremLivre,
+            'livreAuteur' => $livreAuteur,
+            'genreRoman' => $genreRoman,
+            'sf' => $sf,
+            'livre2' => $livre2,
+        ]);
+    }
+
+    #[Route('/emprunteur', name: 'app_test_emprunteur')]
+    public function emprunteur(ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        
+
+        return $this->render('test/emprunteur.html.twig', [
+            
+        ]);
+    }
+
+//     Requêtes de lecture :
+
+// - la liste complète des emprunteurs, triée par ordre alphabétique de nom et prénom
+// - les données de l'emprunteur dont l'id est `3`
+// - les données de l'emprunteur qui est relié au user dont l'id est `3`
+// - la liste des emprunteurs dont le nom ou le prénom contient le mot clé `foo`, triée par ordre alphabétique de nom et prénom
+// - la liste des emprunteurs dont le téléphone contient le mot clé `1234`, triée par ordre alphabétique de nom et prénom
+// - la liste des emprunteurs dont la date de création est antérieure au 01/03/2021 exclu (c-à-d strictement plus petit), triée par ordre alphabétique de nom et prénom
+
 }
--- src/Repository/LivreRepository.php
+++ src/Repository/LivreRepository.php
@@ -21,6 +21,69 @@ class LivreRepository extends ServiceEntityRepository
         parent::__construct($registry, Livre::class);
     }
 
+    /**
+    * @return Livre[] Returns an array of Livre objects
+    */
+    public function findAll(): array
+    {
+        return $this->createQueryBuilder('l')
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * This method find all books containing one title
+     * @param string $titre The titre to search for
+     * @return Livre[] Returns an array of Livre objects
+    */
+    public function findBookByTitle(?string $titre): array
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.titre LIKE :val')
+            ->setParameter('val', "%$titre%")
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * This method finds books write by an autor
+     * @param int $auteurid The id of the auteur for which we want to find the books
+     * @return Livre[] Returns an array to Livre objects
+    */
+    public function findByAuteur(?int $auteurid): array
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.auteur = :val')
+            ->setParameter('val', $auteurid)
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+        // Liste des livres dont le genre contient le mot clé `roman`, triée par ordre alphabétique de titre
+
+    /**
+     * This method finds books with a genre
+     * @param string $genre The genre of the book for which we want to find the books
+     * @return Livre[] Returns an array to Livre objects
+    */
+    public function findByGenre(?string $genre)
+    {
+        return $this->createQueryBuilder('l')
+            ->innerJoin('l.genres', 'g')
+            ->andWhere('g.nom LIKE :nom')
+            ->setParameter('nom', "%$genre%")
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    
 //    /**
 //     * @return Livre[] Returns an array of Livre objects
 //     */
